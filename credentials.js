dbffd83ef33a4e1ecd5bc5532fa3f50ec5256e1b
NathanJPhillips <github@nathanphillips.co.uk>
2016-12-21 21:51:58 +0000

    Moved the credential header setup to credentials.js

--- credentials.js
+++ credentials.js
@@ -1,2 +1,8 @@
 var username = "NathanJPhillips";
 var accessCode = "password";
+
+$.ajaxSetup({
+  beforeSend: function (xhr) {
+    xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + accessCode));
+  }
+});
