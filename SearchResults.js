c65b5470473bbb329ed01f9df2d241b1e64c7eb6
NathanJPhillips <nathan.phillips@diffblue.com>
2016-12-23 11:49:37 +0000

    Tidy view model
    Used extenders for formatting styles
    Renamed view model members

--- SearchResults.js
+++ SearchResults.js
@@ -1,4 +1,3 @@
-function msToDaysTo1dp(ms) { return Math.round(ms / 1000 / 60 / 60 / 24 * 10) / 10; }
 function sum(collection) { return collection.reduce(function (sum, x) { return sum + x }, 0); }
 function min(collection) { return collection.reduce(function (min, x) { return x > min ? min : x; }, NaN); }
 function max(collection) { return collection.reduce(function (max, x) { return x < max ? max : x; }, NaN); }
@@ -7,28 +6,28 @@ function average(collection) { return sum(collection) / collection.length; }
 function SearchResults() {
   var self = this;
 
-  self.items = ko.observableArray([]);
-  self.totalCount = ko.observable(0);
-  self.itemAges = function () { return self.items().map(function (item) { return new Date() - new Date(item.created_at); }); };
-  self.averageAge = ko.computed(function () { return msToDaysTo1dp(average(self.itemAges())); });
-  self.minAge = ko.computed(function () { return msToDaysTo1dp(min(self.itemAges())); })
-  self.maxAge = ko.computed(function () { return msToDaysTo1dp(max(self.itemAges())); })
+  self.pullRequests = ko.observableArray([]);
+  self.totalPRCount = ko.observable(0);
+  self.pullRequestAges = function () { return self.pullRequests().map(function (item) { return new Date() - new Date(item.created_at); }); };
+  self.averagePRAge = ko.computed(function () { return average(self.pullRequestAges()); });
+  self.minPRAge = ko.computed(function () { return min(self.pullRequestAges()); })
+  self.maxPRAge = ko.computed(function () { return max(self.pullRequestAges()); })
 
   self.errorMessage = ko.observable();
   self.activeRequests = ko.observable(0);
   self.loading = ko.computed(function () { self.activeRequests() == 0; });
-  self.uninitialised = ko.computed(function () { return self.items().length == 0; });
+  self.uninitialised = ko.computed(function () { return self.pullRequests().length == 0; });
 
   self.update =
     function () {
-      var items = [];
+      var pullRequests = [];
       // Function to load a page of results
       function getPage(uri) {
         self.activeRequests(self.activeRequests() + 1);
         $.getJSON(uri, function (data, textStatus, jqXHR) {
-          self.totalCount(data.total_count);
-          // Get the items
-          items = items.concat(data.items);
+          self.totalPRCount(data.total_count);
+          // Get the pull requests
+          pullRequests = pullRequests.concat(data.items);
           // Get the link to the next page of results
           var nextLinkSuffix = "; rel=\"next\"";
           var nextLinks = jqXHR.getResponseHeader("Link").split(",").filter(function (link) { return link.endsWith(nextLinkSuffix); });
@@ -36,12 +35,13 @@ function SearchResults() {
             var nextLink = nextLinks[0];
             nextLink = nextLink.substring(1, nextLink.length - nextLinkSuffix.length - 1);
             getPage(nextLink);
-            return;
+            return;   // Don't update until read all pages
           }
-          if (items.length != self.totalCount())
+          if (pullRequests.length != self.totalPRCount())
             self.errorMessage("Couldn't get all the PRs");
-          self.items(items);
-        }).always(function () { self.activeRequests(self.activeRequests() - 1); });
+          self.pullRequests(pullRequests);
+        })
+        .always(function () { self.activeRequests(self.activeRequests() - 1); });
       }
       // Load the first page
       getPage("https://api.github.com/search/issues?q=user%3Adiffblue+type%3Apr+is%3Aopen");
---
+++ knockout-extenders.js
@@ -0,0 +1,34 @@
+ko.extenders.numeric = function(target, precision) {
+	var result = ko.dependentObservable({
+		read: function() {
+			return target().toFixed(precision);
+		},
+		write: target
+	});
+	result.raw = target;
+	return result;
+};
+
+ko.extenders.dateUnits = function(target, units) {
+	var result = ko.dependentObservable({
+		read: function() {
+			var result = target();
+			switch (units) {
+				case "weeks":
+					result /= 7;
+				case "days":
+					result /= 24;
+				case "hours":
+					result /= 60;
+				case "minutes":
+					result /= 60;
+				case "seconds":
+					result /= 1000;
+			}
+			return result;
+		},
+		write: target
+	});
+	result.raw = target;
+	return result;
+};
