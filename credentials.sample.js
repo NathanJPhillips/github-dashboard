79af0494664fcab43b6801bc9bf395f64da6f243
Nathan Phillips <NathanJPhillips@users.noreply.github.com>
2017-01-17 14:37:32 +0000

    Update credentials.sample.js

--- credentials.sample.js
+++ credentials.sample.js
@@ -1,7 +1,7 @@
 // Copy this file as credentials.js, and edit the below lines
 
 // Your github username
-var username = "NathanJPhillips";
+var username = "username";
 
 // A personal access token from https://github.com/settings/tokens
 var accessCode = "token";
